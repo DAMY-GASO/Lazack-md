927a5d5e4145bedc477e111f0a588294841bc373
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-04-30 01:32:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,50 +1,46 @@
-const toBool = (x) => x == 'true'
-const { existsSync } = require('fs')
 const { Sequelize } = require('sequelize');
-if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
-process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
-const DB_URL =  process.env.DATABASE_URL || '';
+const fs = require('fs');
+
+if (fs.existsSync('config.env')) {
+  require('dotenv').config({
+      path: './config.env'
+  });
+}
+
+const toBool = (x) => x === 'true';
+
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
+
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
-    HEROKU: {
-        API_KEY: process.env.HEROKU_API_KEY,
-        APP_NAME: process.env.HEROKU_APP_NAME
-    },
-    PORT: process.env.PORT || 3000,
-    BASE_URL : "https://upper-romy-inrl-bot.koyeb.app/",
-    REPO: "Lazack28/Lazack-md",
-    BGM_URL : process.env.BGM_URL || "null",
-    REJECT_CALL : toBool(process.env.REJECT_CALL || 'false'),
-    BADWORD_BLOCK : toBool(process.env.BADWORD_BLOCK || 'false'),
-    ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "false"),
-    PM_BLOCK : toBool(process.env.PM_BLOCK || "false"),
-    BGMBOT : toBool(process.env.BGMBOT || "false"),
-    CALL_BLOCK : toBool(process.env.CALL_BLOCK || "false"),
-    STATUS_VIEW : process.env.STATUS_VIEW || "false",
-    SAVE_STATUS : toBool(process.env.SAVE_STATUS || "false"),
-    ADMIN_SUDO_ACCESS: toBool(process.env.ADMIN_SUDO_ACCESS || "false"),
-    DISABLE_PM: toBool(process.env.DISABLE_PM || "false"),
-    DISABLE_GRP : toBool(process.env.DISABLE_GRP || "false"),
-    ERROR_MSG : toBool(process.env.ERROR_MSG || "true"),
-    AJOIN: toBool(process.env.AJOIN || 'false'),
-    READ : process.env.READ ||  "false",//true, command
-    CHATBOT : process.env.CHATBOT || "false",//true, pm, group
-    REACT : process.env.REACT || "false",//true, command, emoji
-    WARNCOUND : process.env.WARNCOUND || 5,
-    BOT_INFO : process.env.BOT_INFO || "INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
-    WORKTYPE : process.env.WORKTYPE || "private",
-    PREFIX : process.env.PREFIX || "[.,!]",//both  .  and [.] equal, for multi prefix we use [] this
-    LANG : process.env.LANG || "en",
-    PERSONAL_MESSAGE: process.env.PERSONAL_MESSAGE || "null",
-    BOT_PRESENCE : process.env.BOT_PRESENCE || "unavailable",
-    AUDIO_DATA : process.env.AUDIO_DATA || "INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
-    STICKER_DATA : process.env.STICKER_DATA || "inrl;inrl",
-    BRAINSHOP: process.env.BRAINSHOP || '172372,nbjE0YAlyw3cpoMl',
-    SUDO : process.env.SUDO || "null",
-    RMBG_KEY: process.env.RMBG_KEY,
-    OPEN_AI: process.env.OPEN_AI,
-    ELEVENLABS: process.env.ELEVENLABS,
-    INRL_KEY: process.env.INRL_KEY || 'free50_inrl',
-    OCR_KEY: (process.env.OCR_KEY || 'K84003107488957').trim(),
-    DATABASE: DB_URL ? new Sequelize(DB_URL,{dialect:'postgres',ssl:true,protocol: 'postgres', dialectOptions: {native: true,ssl:{require: true,rejectUnauthorized: false}}, logging: false}) : new Sequelize({dialect:'sqlite',storage:'./database.db',logging:false}) 
+  BAN_CHATS: process.env.BAN_CHATS || "",
+  PORT: toBool(process.env.PORT) || 8000,
+  AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
+  WARN_COUNT: process.env.WARN_COUNT || '3',
+  ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_dc0d_6c73_8723_f460_6731_bc18_55c3_d33b',
+  LANG: process.env.LANG || 'EN',
+  ELEVENLABS: process.env.ELEVENLABS,
+  HANDLERS: process.env.HANDLER  || '/',
+  ALLWAYS_ONLINE: toBool(process.env.ALLWAYS_ONLINE || "false"),
+  READ_MSG: toBool(process.env.READ_MSG || "false"),
+  BRANCH: "main",
+  KOYEB_API: process.env.KOYEB_API,
+  BRAINSHOP: process.env.BRAINSHOP || '172372,nbjE0YAlyw3cpoMl',
+  TGTOKEN: "bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4",
+  API: 'https://api.lokiser.xyz/',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'ᴊᴀʀᴠɪꜱ;ᴡᴀʙᴏᴛ',
+  CALL_BLOCK: toBool(process.env.CALL_BLOCK) || false,
+  SAVE_STATUS: toBool(process.env.SAVE_STATUS) || false,
+  STATUS_VIEW: process.env.STATUS_VIEW || "true",
+  REJECT_CALL: toBool(process.env.REJECT_CALL || "true"),
+  ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
+  WELCOME_MSG: process.env.WELCOME_MSG || "Hi &mention Welcome to &gname",
+  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi $mention It was Nice Seeing you",
+  MEDIA_DATA: process.env.MEDIA_DATA|| 'ʟɪꜱᴛ ᴍᴇɴᴜ;ᴊᴀʀᴠɪꜱ-ᴍᴅ;https://i.imgur.com/ltPPMTh.jpeg',
+  SUDO: process.env.SUDO || '255734980103',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
+  BOT_INFO: process.env.BOT_INFO || 'ᴊᴀʀᴠɪꜱ;ʟᴏᴋɪ-xᴇʀ;https://i.imgur.com/VIpl3q5.mp4',
+  WORK_TYPE: process.env.WORK_TYPE || 'private',
+  DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
 };
